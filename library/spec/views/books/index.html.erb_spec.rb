8758a3213340b1aee83fbe1330833898c5b0356d
Matjaz <vmatkov@gmail.com>
2015-10-14 22:44:34 +0200

    Added RSPEC test for books_controller, categories_controller, books/index, show view and categories/index view ... popravek

--- library/spec/views/books/index.html.erb_spec.rb
+++ library/spec/views/books/index.html.erb_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe "books/index", type: :view do
   context "with 2 books" do
     before(:each) do
       assign(:books, [
-        Book.create!(:name => "Other"),
-        Book.create!(:name => "Fantasy")
+        Book.create!(:name => "Distorzija", :category_id => 0),
+        Book.create!(:name => "Fantasy land", :category_id => 0)
       ])
     end
 
     it "displays both books" do
       render
 
-      expect(rendered).to match /Other/
-      expect(rendered).to match /Fantasy/
+      expect(rendered).to match /Distorzija/ and /Other/ #creating two books that display on index page.
+      expect(rendered).to match /Fantasy/ and /Other/ #they must contain book name and category
     end
   end
 end
